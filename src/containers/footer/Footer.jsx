fa90058e096a336ba67db92bbfad7c4eca8a4e9a
KDraxler <kdraxler0000@gmail.com>
2022-07-16 20:24:38 +0700

    update

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -11,11 +11,11 @@ const Footer = () => {
     footerAPI().find().then((res) => {
       setFooterData(res.data);
       setLoading(false);
-      console.log(res.data)  
-    })
+    }) 
   },[])
   
   if(loading) return <h1>Loading</h1>
+  // console.log(footerData)  
   return (
     <div className='refly__footer section__padding'>
       <div className='refly__footer-content'>
@@ -24,24 +24,35 @@ const Footer = () => {
             <img src={Logo} alt="DesignbyRefly" />
             <p>Designbyrefly</p>
           </div>
-          <p className='refly__footer-content__home-desc'>Welcome to my Portfolio and I hope we can work together</p>
+          <p className='refly__footer-content__home-desc'>{footerData.attributes.description}</p>
         </div>
         <div className='refly__footer-content__navigation'>
           <h2 className='refly__footer-content__navigation-title'>Navigation</h2>
           <div className='refly__footer-content__navigation-content'>
-            <a href=""><p>Home</p></a>
+            {
+              footerData.attributes.menus.data === null ? <p>No data</p> : footerData.attributes.menus.data.map(menu => {
+                return(
+                  <a href="#"><p>{menu.attributes.title}</p></a>
+                )
+              })
+            }
+            {/* <a href=""><p>Home</p></a>
             <a href=""><p>About Us</p></a>
             <a href=""><p>Project</p></a>
-            <a href=""><p>Contact Us</p></a>
+            <a href=""><p>Contact Us</p></a> */}
           </div>
         </div>
         <div className='refly__footer-content__social'>
           <h2 className='refly__footer-content__navigation-title'>Social Media</h2>
           <div className='refly__footer-content__navigation-content'>
-            <a href="#"><p>Dribble</p></a>
-            <a href="#"><p>Instagram</p></a>
-            <a href="#"><p>LinkedIn</p></a>
-            <a href="#"><p>Twitter</p></a>
+            {
+              footerData.attributes.links.data === null ? <p>No data</p> : footerData.attributes.links.data.map(link => {
+                return(
+                  <a href={link.attributes.link}><p>{link.attributes.title}</p></a>
+                )
+              })
+            }
+           
           </div>
         </div>
       </div>
